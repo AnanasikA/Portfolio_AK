28ddebe59150b4ada7ada3736249ef0f99910adf
AnanasikA <valievaanastasia98@gmail.com>
2026-06-11 19:56:47 +0200

    PricingBanner

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <Hero />
         <ClientMarquee />
         <FeaturedProjects />
-        <Pricing />
+        <PricingBanner />
         <ProcessSection />
         <WhyUs />
         <FAQ />
